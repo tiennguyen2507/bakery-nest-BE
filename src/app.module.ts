455c9286a10c39fc19c9e48d2b5f826a2ff74698
tien <nguyenledinhtien2507@gmail.com>
2023-10-14 16:32:21 +0700

    check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { dataSourceOptions } from 'db/data-source';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { UserModule } from './user/user.module';
+// import { dataSourceOptions } from 'db/data-source';
+// import { AuthModule } from './auth/auth.module';
+// import { ConfigModule } from '@nestjs/config';
 
-const DataBaseModule = TypeOrmModule.forRoot(dataSourceOptions);
+// const DataBaseModule = TypeOrmModule.forRoot(dataSourceOptions);
 
 @Module({
-  imports: [DataBaseModule, UserModule, AuthModule, ConfigModule.forRoot()],
+  // imports: [UserModule, AuthModule, ConfigModule.forRoot()],
+
   controllers: [AppController],
   providers: [AppService],
 })
