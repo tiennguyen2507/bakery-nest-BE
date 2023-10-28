0fb2c6fed1bdde1b418b5acc0e38255981a9a330
tien <nguyenledinhtien2507@gmail.com>
2023-10-28 15:50:33 +0700

    test 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { dataSourceOptions } from 'db/data-source';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
 
-// const DataBaseModule = TypeOrmModule.forRoot(dataSourceOptions);
+const DataBaseModule = TypeOrmModule.forRoot(dataSourceOptions);
 
 @Module({
-  // imports: [DataBaseModule],
+  imports: [DataBaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
