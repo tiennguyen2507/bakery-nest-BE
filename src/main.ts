116036ecc4241870d928adb085d237f7ec4d5267
tiennguyen2507 <86944158+tiennguyen2507@users.noreply.github.com>
2023-11-03 22:52:18 +0700

    Update main.ts 2

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
   const config = new DocumentBuilder()
     .setTitle('Blog APIs')
     .setDescription('list APIs for simple')
