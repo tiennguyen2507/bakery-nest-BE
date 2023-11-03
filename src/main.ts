f38cf3b2df66868851839b768e2726a952ae299c
tiennguyen2507 <86944158+tiennguyen2507@users.noreply.github.com>
2023-11-03 22:43:48 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,6 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://YOUR-APP-NAME.vercel.app', 'http://localhost:3000', '*'],
-    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-    credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-  });
   const config = new DocumentBuilder()
     .setTitle('Blog APIs')
     .setDescription('list APIs for simple')
