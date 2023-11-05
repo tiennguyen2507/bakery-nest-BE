3381d0de10f665b8fd29b3e606498e2b97b2a9cc
tien <nguyenledinhtien2507@gmail.com>
2023-11-05 21:44:27 +0700

    cor

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Blog APIs')
     .setDescription('list APIs for simple')
