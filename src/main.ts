ec5edd4ad88c7454ed01b4caf84231235a32b0c9
HungHuynh <manhhung05bn@gmail.com>
2024-04-28 23:20:43 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['http://localhost:3000'] });
   swaggerConfig(app);
-  await app.listen(4000);
+  await app.listen(3000);
 }
 bootstrap();
